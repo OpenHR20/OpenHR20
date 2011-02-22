d0d03fb57af3bf36bf6bf1d0b7969a1c0ca013b8
dobryj <dobryj@e6282f77-c345-0410-9e5f-9d7f1dee61d8>
2011-02-22 20:28:38 +0000

    use flot for charts
    git-svn-id: https://openhr20.svn.sourceforge.net/svnroot/openhr20@288 e6282f77-c345-0410-9e5f-9d7f1dee61d8

--- rfmsrc/frontend/www/contend/status.php
+++ rfmsrc/frontend/www/contend/status.php
@@ -80,7 +80,64 @@ class contend_status extends contend {
 	echo '<input type="submit" value="Submit">';
 	echo "</form>";
       }
-      echo "<div><img src=\"/chart.php?addr=$this->addr&real=1&wanted=1&valve=1&hours=$chart_hours\" \></div>";
+      ?>
+      <!--[if IE]><script language="javascript" type="text/javascript" src="/js/excanvas.min.js"></script><![endif]-->
+      <script language="javascript" type="text/javascript" src="/js/jquery.min.js"></script>
+      <script language="javascript" type="text/javascript" src="/js/jquery.flot.min.js"></script>
+      
+      <script id="source" language="javascript" type="text/javascript">
+      document.write("<div id=\"placeholder\" style=\"width:800px;height:400px;\"></div>");
+      $(function () {
+	<?php
+	    $now = time();
+	    $min_time = $now-$chart_hours*60*60;
+	    
+	    $result = $db->query("SELECT * FROM log WHERE addr=$this->addr AND time>$min_time ORDER BY time");    
+	    $real=array();$wanted=array();$valve=array();$markings=array();
+	    $off=date_offset_get(new DateTime);
+	    $window=-1; $win_pos=0;
+	    while ($row = $result->fetch()) {                                                                                            
+		$t = ($row['time']+$off)."000";
+	        array_push($real,array($t,$row['real']/100));                                                                              
+	        array_push($wanted,array($t,$row['wanted']/100));                                                                              
+	        array_push($valve,array($t,$row['valve']));                                                                              
+		if ($window != $row['window']) {
+		    if ($window == 1) {
+		    	array_push($markings,
+			    array('xaxis' => array ( 'from' => $win_pos , 'to' => $t ),
+			    'color' => '#e0e0ff'));
+		    }
+		    $window = $row['window'];
+		    $win_pos = $t;
+		}
+	    }       
+	    if ($window == 1) {
+	    	array_push($markings,
+		    array('xaxis' => array ( 'from' => $win_pos , 'to' => $t ),
+		    'color' => '#e0e0ff'));
+	    }
+	    echo "var real = ". json_encode($real).";\n";
+	    echo "var wanted = ". json_encode($wanted).";\n";
+	    echo "var valve = ". json_encode($valve).";\n";
+	    echo "var markings = ". json_encode($markings).";\n";
+	    
+	?>
+	    $.plot($("#placeholder"),
+	       [ { data: wanted },
+	         { data: real },
+	         { data: valve, yaxis: 2 }],
+	       { 
+	            xaxis: { mode: 'time' },
+		    yaxis: { tickFormatter: function (v, axis) { return v.toFixed(axis.tickDecimals) +"॰C" }},
+		    y2axis: { tickFormatter: function (v, axis) { return v.toFixed(axis.tickDecimals) +"%" }},
+		    grid: {  markings: markings },
+		    colors: ["#00ff00", "#ff0000", "#0000ff"]
+	        });
+	});
+      </script>												   
+	      
+      <?php
+      echo "<noscript><div><img src=\"/chart.php?addr=$this->addr&real=1&wanted=1&valve=1&hours=$chart_hours\" \></div></noscript>";
       echo '<div><span style="color: red;">Real temperature</span> <span style="color: green;">Wanted temperature</span> <span style="color: blue;">Valve position</span></div>';
       $result = $db->query("SELECT * FROM versions WHERE addr=$this->addr LIMIT 1");
       if ($row = $result->fetch()) {
