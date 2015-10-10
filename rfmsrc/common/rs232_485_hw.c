OpenHR20/OpenHR20

commit 335beec1f78ff5d07c62362b89af5b03e8fc5fb3

don't enable the serial receiver when compiled without serial option

rfmsrc/common/rs232_485_hw.c
@@ -161,10 +161,11 @@ void RS_Init(void)
             #if defined(MASTER_CONFIG_H)
 			  UCSR0B = _BV(RXCIE0) | _BV(RXEN0);
 			#endif
-        #else 
-            UCSR0B = _BV(RXCIE0) | _BV(RXEN0);
+			// todo: decide between 2way RS232 and one way debugging only
+            #elif (defined COM_RS232) || (defined COM_RS485) 
+                UCSR0B = _BV(RXCIE0) | _BV(RXEN0);
+            #endif
             UCSR0C = (_BV(UCSZ00) | _BV(UCSZ01));     // Asynchron 8N1
-        #endif
 	#if !defined(_AVR_IOM16_H_) && !defined(_AVR_IOM32_H_) && !defined(_AVR_IOM328P_H_)
     PCMSK0 |= (1<<PCINT0); // activate interrupt
   #endif

commit 3153f44dcda9eac67c6c5fc3eeb14e9fb89a8db4

fix the RS232 ifdefs for master (broken in 335beec)

rfmsrc/common/rs232_485_hw.c
@@ -161,11 +161,13 @@ void RS_Init(void)
             #if defined(MASTER_CONFIG_H)
 			  UCSR0B = _BV(RXCIE0) | _BV(RXEN0);
 			#endif
+		#else
 			// todo: decide between 2way RS232 and one way debugging only
-            #elif (defined COM_RS232) || (defined COM_RS485) 
+            #if (defined COM_RS232) || (defined COM_RS485) 
                 UCSR0B = _BV(RXCIE0) | _BV(RXEN0);
             #endif
             UCSR0C = (_BV(UCSZ00) | _BV(UCSZ01));     // Asynchron 8N1
+		#endif
 	#if !defined(_AVR_IOM16_H_) && !defined(_AVR_IOM32_H_) && !defined(_AVR_IOM328P_H_)
     PCMSK0 |= (1<<PCINT0); // activate interrupt
   #endif