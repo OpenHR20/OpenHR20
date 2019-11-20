cd1a04c2f602f5a7884755541acf837643cdf563
fra87 <5077242+fra87@users.noreply.github.com>
2019-11-20 20:11:01 +0100

    Improved compilation scripts * For the unit compilation scripts, the RFM frequency was set variable, and it was added the possibility to call the script from another folder * For the master compilation scripts, the relevant variables were made configurable like the unit compilation one * Scripts were renamed to reflect the target rather than a specific HW

--- src/compile_unit.sh
+++ src/compile_unit.sh
@@ -4,6 +4,9 @@
 set -o nounset
 set -o errexit
 
+# save script directory (to launch makefile in this folder)
+SCRIPTPATH="${0%/*}"
+
 # defaults
 ADDR=
 REMOTE_ONLY=0
@@ -16,6 +19,7 @@ KEY4=89
 KEY5=ab
 KEY6=cd
 KEY7=ef
+RFM_FREQ=868
 
 while [ "$#" -ne 0 ]; do
 	if [ "$1" == "--addr" ]; then
@@ -41,9 +45,11 @@ while [ "$#" -ne 0 ]; do
 		KEY7="${HASHED:14:2}"
 	elif [ "$1" == "--remoteOnly" ]; then
 		REMOTE_ONLY=1; shift;
+	elif [ "$1" == "--freq" ]; then
+		RFM_FREQ=$2; shift; shift;
 	else
 		echo "unknown options starting at $*"
-		echo "e.g. $0 [--key <k0> <k1> <k2> <k3> <k4> <k5> <k6> <k7>] [--HW HR20|HR25|...] [--addr <addr>] [-remoteOnly] [--pass <passphrase>]"
+		echo "e.g. $0 [--key <k0> <k1> <k2> <k3> <k4> <k5> <k6> <k7>] [--HW HONEYWELL|HR25|THERMOTRONIC] [--addr <addr>] [--freq <freq>] [-remoteOnly] [--pass <passphrase>]"
 		[ "$REMOTE_ONLY" != "0" ] && REMOTE_ONLY_STRING="-remoteOnly" || REMOTE_ONLY_STRING=""
 		[ -z "$ADDR" ] && ADDR="<Not set>"
 		echo "defaults: --key $KEY0 $KEY1 $KEY2 $KEY3 $KEY4 $KEY5 $KEY6 $KEY7 --HW $HW --addr $ADDR $REMOTE_ONLY_STRING"
@@ -56,10 +62,11 @@ if [ -z "$ADDR" ]; then
 	exit 1;
 fi
 
-make clean HW=$HW
-make RFM=1 \
+make -C ${SCRIPTPATH} clean HW=$HW
+make -C ${SCRIPTPATH} \
+RFM=1 \
 RFM_TUNING=1 \
-RFM_FREQ_MAIN=868 \
+RFM_FREQ_MAIN=${RFM_FREQ} \
 RFM_FREQ_FINE=0.35 \
 RFM_BAUD_RATE=19200 \
 SECURITY_KEY_0=0x${KEY0} \
@@ -75,4 +82,3 @@ HW=${HW} \
 RFM_WIRE=MARIOJTAG \
 REMOTE_SETTING_ONLY=${REMOTE_ONLY} \
 MOTOR_COMPENSATE_BATTERY=1
-
